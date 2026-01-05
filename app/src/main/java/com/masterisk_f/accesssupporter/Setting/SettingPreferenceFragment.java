bd7f748da9b1141faf1a82f760b285a4e713a8fe
masterisk <Masterisk-F@users.noreply.github.com>
2026-01-05 19:55:44 +0900

    feat: データバージョンを設定画面に表示

--- app/src/main/java/com/masterisk_f/accesssupporter/Setting/SettingPreferenceFragment.java
+++ app/src/main/java/com/masterisk_f/accesssupporter/Setting/SettingPreferenceFragment.java
@@ -11,5 +11,22 @@ public class SettingPreferenceFragment extends PreferenceFragment {
 		super.onCreate(savedInstanceState);
 		
 		addPreferencesFromResource(R.xml.preference);
+
+		findPreference("data_version").setSummary(getDataVersion());
+	}
+
+	private String getDataVersion() {
+		try {
+			java.io.InputStream is = getActivity().getAssets().open("latest_info.json");
+			byte[] buffer = new byte[is.available()];
+			is.read(buffer);
+			is.close();
+			String json = new String(buffer, "UTF-8");
+			org.json.JSONObject jsonObject = new org.json.JSONObject(json);
+			return String.valueOf(jsonObject.getInt("version"));
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "不明";
+		}
 	}
 }
