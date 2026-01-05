a48e3be9d0515aeb9d0cb9bbb2865cfbdf31e48d
masterisk <Masterisk-F@users.noreply.github.com>
2026-01-05 16:48:56 +0900

    migrate: AndroidXへの移行

--- app/src/main/java/com/masterisk_f/accesssupporter/Tabs/StationsListFragment.java
+++ app/src/main/java/com/masterisk_f/accesssupporter/Tabs/StationsListFragment.java
@@ -3,7 +3,7 @@ package com.masterisk_f.accesssupporter.Tabs;
 import android.content.Context;
 import android.location.Location;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import androidx.fragment.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
