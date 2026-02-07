65b6482119eebe122095fc56949a2c63e78ee3f1
masterisk <Masterisk-F@users.noreply.github.com>
2026-02-07 20:16:39 +0900

    chore: comment update

--- app/src/main/java/com/masterisk_f/accesssupporter/MapsActivity.java
+++ app/src/main/java/com/masterisk_f/accesssupporter/MapsActivity.java
@@ -127,14 +127,8 @@ import java.util.List;
 	}
 	
 	/**
-	 * Manipulates the map once available.
-	 * This callback is triggered when the map is ready to be used.
-	 * This is where we can add markers or lines, add listeners or move the camera. In this case,
-	 * we just add a marker near Sydney, Australia.
-	 * If Google Play services is not installed on the device, the user will be prompted to install
-	 * it inside the SupportMapFragment. This method will only be triggered once the user has
-	 * installed Google Play services and returned to the app.
-	 */
+	 * Mapが利用可能になった際の処理
+\	 */
 	@Override
 	public void onMapReady(MapLibreMap mapLibreMap) {
 		mMap = mapLibreMap;
